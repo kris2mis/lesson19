b9e86c62688e491befcaee5d99e6af72e02801d7
NikolNat <102613704+NikolNat@users.noreply.github.com>
2022-11-01 20:37:18 +0300

    add __str__() and __repr__() methods, remove get info() method

--- rectangle.py
+++ rectangle.py
@@ -10,17 +10,24 @@ class Rectangle:
     def calculate_perimeter(self):
         return (self.a + self.b) * 2
 
-    def get_info(self):
+    # def get_info(self):     # это переходит в функцию __str__
+    #     return f"Rectangle: a = {self.a}, b = {self.b}"
+
+    def __repr__(self):
+        return f"Thecnical information about instsnce"
+
+    def __str__(self):
         return f"Rectangle: a = {self.a}, b = {self.b}"
 
     def __del__(self):
         pass
 
 
-rect1 = Rectangle(10, 20)
-rect2 = Rectangle(b=20, a=10)
-rect3 = Rectangle(20)
-rect4 = Rectangle()
+
+# rect1 = Rectangle(10, 20)
+# rect2 = Rectangle(b=20, a=10)
+# rect3 = Rectangle(20)
+# rect4 = Rectangle()
 # print(rect1.get_info())
 # print(rect2.get_info())
 # print(rect3.get_info())
--- view.py
+++ view.py
@@ -3,12 +3,13 @@
 
 from rectangle import Rectangle
 
+
 class View:
     def convert(rectangles):
         if isinstance(rectangles, (list, tuple)):
             result = "List of rectangles:\n"
             for rect in rectangles:
-                if isinstance( rect, Rectangle):
-                    result+=rect.get_info()+"\n"
+                if isinstance(rect, Rectangle):
+                    result += str(rect) + "\n"
 
             return result
